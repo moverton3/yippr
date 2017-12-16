f3a081b1fb5ec5ad4b06f1b860da2139b1ab65cd
Max Winslow <maxtwinslow1@gmail.com>
2017-12-16 15:49:53 -0700

    new background color

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -22,6 +22,7 @@ class Login extends Component {
     const { email, password } = this.state;
     return (
       <Segment inverted color='teal'>
+        
         <Header as='h1' textAlign='center'>Login</Header>
         <Form onSubmit={this.handleSubmit}>
           <Form.Field>
