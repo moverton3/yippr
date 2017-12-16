dfee80fc41172a17959bc2dabd5127a5ba9a3625
Max Winslow <maxtwinslow1@gmail.com>
2017-12-16 10:31:09 -0700

    Created form in home component

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,10 +1,19 @@
 import React, { Component } from 'react';
-import { Header } from 'semantic-ui-react';
-
+import { Header, Form, Label, TextArea, Button, Segment } from 'semantic-ui-react';
+import { Router } from 'react-router-dom'
 class Home extends Component {
   render() {
     return (
-      <Header as='h1' textAlign='center'>Home Component</Header>
+      <Segment>
+        <Header as='h1'>Create Your Bio</Header>
+        <Form>
+          <Label>Profile Image</Label>
+          <Form.Input placeholder="URL goes here" />
+          <Label>Description</Label>
+          <TextArea placeholder="Description Goes Here" />
+        </Form>
+        <Button secondary>Add Bio</Button>
+      </Segment>
     );
   }
 }
